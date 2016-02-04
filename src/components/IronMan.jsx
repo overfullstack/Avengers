73adc4bbb7e54c81b3e8ff9981ab16dea303f652
gopalakshintala <gopalakshintala@gmail.com>
2016-02-04 12:35:16 +0530

    Init

--- src/components/IronMan.jsx
+++ src/components/IronMan.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class App extends React.component {
+export default class IronMan extends React.Component {
   render(){
     return (
       <div>
         <h1>I'm the Iron Man</h1>
       </div>
     );
-  };
+  }
 }
-
-export default App;
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import IronMan from './components/IronMan.jsx'
+
+ReactDom.render(<IronMan />, document.getElementById('avenger'));
