58388734cc5c1175308e1e8780759da497814461
gopalakshintala <gopalakshintala@gmail.com>
2016-02-04 12:45:16 +0530

    Hulk is not Happy

--- src/components/IronMan.jsx
+++ src/components/IronMan.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 export default class IronMan extends React.Component {
   render(){
     return (
-      <div>
-        <h1>I'm the Iron Man</h1>
-      </div>
+      <h1>I'm the Iron Man</h1>
     );
   }
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import IronMan from './components/IronMan.jsx'
+import Hulk from './components/Hulk.jsx'
 
-ReactDom.render(<IronMan />, document.getElementById('avenger'));
+ReactDom.render(
+  <div>
+    <IronMan />
+    <Hulk />
+  </div> ,
+
+  document.getElementById('avenger'));
