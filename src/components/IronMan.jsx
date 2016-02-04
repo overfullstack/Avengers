05da10e0476bb1d26fbfc04f046a5f887f33f4e3
gopalakshintala <gopalakshintala@gmail.com>
2016-02-04 12:17:34 +0530

    Initial commit

---
+++ src/components/IronMan.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.component {
+  render(){
+    return (
+      <div>
+        <h1>I'm the Iron Man</h1>
+      </div>
+    );
+  };
+}
+
+export default App;
