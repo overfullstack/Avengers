b46ca9037c13a56a313639b6fe5a2e67e2bff5e1
gopalakshintala <gopalakshintala@gmail.com>
2016-02-15 11:30:48 +0530

    1. Optimized response in mutation 2. Search feature with new Query parameter to GraphQL and making it debounce.

--- src/components/Avengers.jsx
+++ src/components/Avengers.jsx
@@ -6,28 +6,51 @@ import mui from 'material-ui';
 var {AppBar, Card, List} = mui;
 import DialogWriter from './DialogWriter.jsx';
 import Relay from 'react-relay';
+import {debounce} from 'lodash';
 
 export default class Avengers extends React.Component {
 
+  search = (e) => {
+    e.persist();
+    console.log(e);
+    this.debounceSearch(e);
+  };
+
+  debounceSearch = debounce((e) => {
+    console.log(e);
+    let query = e.target.value;
+    this.props.relay.setVariables({query});
+  }, 300);
+
   render() {
     var dialogs = this.props.dialogStore.dialogConnection.edges.map((edge, i) => {
-        return (
-          <Hero key={i} dialog={edge.node}/>
-        );
-      });
-
-    let style = {};
-    style.backgroundColor = '#009688';
+      return (
+        <Hero key={i} dialog={edge.node}/>
+      );
+    });
 
     return (
       <div>
         <AppBar
-          style={style}
+          style={{backgroundColor: '#009688'}}
           title="Avengers Jabber"/>
+
+        <textarea
+          onChange={this.search}
+          style={{
+            width: '25%',
+            borderColor: '#D0D0D0',
+            resize: 'none',
+            borderRadius: 3,
+            minHeight: 10,
+            color: '#555',
+            fontSize: 10,
+            outline: 'auto 0px'
+          }}/>
 
         <Card style={{
         flexGrow: 1
-      }}>
+        }}>
           <List>
             {dialogs}
           </List>
@@ -39,11 +62,14 @@ export default class Avengers extends React.Component {
 }
 
 Avengers = Relay.createContainer(Avengers, {
+  initialVariables: {
+    query: ''
+  },
   fragments: {
     dialogStore: () => Relay.QL `
       fragment on Store {
         id,
-        dialogConnection(first: 100) {
+        dialogConnection(first: 100, query: $query) {
           edges {
             node {
               ${Hero.getFragment('dialog')}
